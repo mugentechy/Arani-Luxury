60fbdabb34d6552d5bcd28d9962b5b4b2016d047
mugentechy <mugentechy@gmail.com>
2024-04-14 14:51:18 +0300

    update

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../assets/nav.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Modal from '@mui/material/Modal';
 import { FaCartShopping } from "react-icons/fa6";
+import '../assets/nav.css'; // Import custom CSS for navigation styling
 
-const Nav = () => {
+const Navigation = () => {
   const navigate = useNavigate();
 
   const handleSignOut = async () => {
@@ -31,34 +33,27 @@ const Nav = () => {
   }, []);
 
   return (
-    <div className="nav-bar">
-      <div className="logo-container">
-        <h2 className="nav_logo">Bags</h2>
-      </div>
-
-      <div className="link-container">
-        <a href="/gallery" className="link">
-          <span>Products</span>
-        </a>
-        <a href="/about" className="link">
-          <span>About Us</span>
-        </a>
-        <a href="/delivery" className="link">
-          <span>Shipping & Delivery</span>
-        </a>
-        <a href="/contact" className="link">
-          <span>Contact</span>
-        </a>
-      </div>
-
-      <div type="button" onClick={handleOpen} className="button-container">
-        <a href="/cart" className="link">
-          <FaCartShopping className="nav_logo" />
-          {cartItemCount > 0 && <span className="cart-item-count">{cartItemCount}</span>} {/* Display cart item count if it's greater than 0 */}
-        </a>
-      </div>
-    </div>
+    <Navbar bg="light" expand="lg" className="container nav-bar">
+      <Navbar.Brand href="/" className="logo-container">
+        <h3 className="nav_logo">Bags</h3>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto align-items-center link-container">
+          <Nav.Link href="/gallery">Products</Nav.Link>
+          <Nav.Link href="/about">About Us</Nav.Link>
+          <Nav.Link href="/delivery">Shipping & Delivery</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+        <Nav className="button-container">
+          <Nav.Link onClick={handleOpen} className="cart-icon">
+            <FaCartShopping className="nav_logo" />
+            {cartItemCount > 0 && <span className="cart-item-count">{cartItemCount}</span>}
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-export default Nav;
+export default Navigation;
